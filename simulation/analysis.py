6b73bac3595c6610a55626ba8b4f41f244c617ce
Chen Gu <c.gu.warwick@gmail.com>
2019-06-05 16:45:17 +0000

    Work on making graphs

--- simulation/analysis.py
+++ simulation/analysis.py
@@ -1,16 +1,18 @@
 import math
 from collections import Counter
+import matplotlib.pyplot as plt
 
 # Calculate the number of messages per second based on the message sent 
 def calculateMessagePerSecond(timeList):
 	time = []
 	for i in timeList:
 		time.append(math.floor(i))
-	Counter(time)
-	print(Counter(time))
+	
+	return dict(Counter(time))
+	#print(dict(Counter(time)))
 
 # Calculate the number of cars per second based on the car's ID from message sent.
-def calculateCarPerSecond(timeList, myVehicleList, receivedVehicleList):
+def calculateVehiclePerSecond(timeList, myVehicleList, receivedVehicleList):
 	time = []
 	newList = []
 	last = 0
@@ -18,15 +20,17 @@ def calculateCarPerSecond(timeList, myVehicleList, receivedVehicleList):
 	timeVehicleList = []
 	for i in timeList:
 		time.append(math.floor(i))
+
 	for i in list(zip(time, myVehicleList, receivedVehicleList)):
 		if i[0] == last:
 			del newList[-1]
 			newList.append(list(set(i+lastItemList)))
+			lastItemList = tuple(set(i+lastItemList))
 		else:
 			newList.append(i)
+			lastItemList = i
 		last = i[0]
-		lastItemList = i
-	#print(newList)
+
 	for i in newList:
 		for m in i:
 			if isinstance(m, float):
@@ -37,6 +41,32 @@ def calculateCarPerSecond(timeList, myVehicleList, receivedVehicleList):
 	#print(timeVehicleList)
 	return timeVehicleList
 
+def drawMessageAndVehiclePerSecond(m, v):
+	x_time = []
+	y_vehicle = []
+
+	x_time2 = []
+	y_message = []
+	for i in v:
+		x_time.append(i.keys()[0])
+		y_vehicle.append(i.values()[0])
+
+	for i in m.keys():
+		x_time2.append(i)
+
+	for i in m.values():
+		y_message.append(i)
+	
+	#print(x_time)
+	#print(y_vehicle)
+ 
+	plt.plot(x_time, y_vehicle, linestyle='dashed', label = "The number of vehicles")
+	plt.plot(x_time2, y_message, label = "The number of messages") 
+  
+	plt.xlabel('Time (Second)') 
+  
+	plt.legend()
+	plt.show()
 
 with open("results.txt", "r") as results:
 	next(results)
@@ -48,6 +78,9 @@ with open("results.txt", "r") as results:
 		timel.append(float(token[0]))
 		myVehiclel.append(int(token[1]))
 		receivedVehiclel.append(int(token[2]))
-		
+
 	#calculateMessagePerSecond(timel)
-	calculateCarPerSecond(timel, myVehiclel, receivedVehiclel)
+	#calculateVehiclePerSecond(timel, myVehiclel, receivedVehiclel)
+
+	drawCarPerSecond(calculateMessagePerSecond(timel), calculateVehiclePerSecond(timel, myVehiclel, receivedVehiclel))
+
